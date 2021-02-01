bf260a7f7d33510ca6e267397a6347d420e2bfcb
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-02-01 05:43:56 -0300

    Iniciando Projeto

---
+++ NewScreenFinance/NewScreenFinance/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace NewScreenFinance
+{
+    partial class frmFinances
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFinaceiro = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnCadastrar = new MaterialSkin.Controls.MaterialFlatButton();
+            this.lblAlmoço = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(50)))), ((int)(((byte)(56)))));
+            this.panel1.Controls.Add(this.btnFinaceiro);
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Location = new System.Drawing.Point(0, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(150, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnFinaceiro
+            // 
+            this.btnFinaceiro.AutoSize = true;
+            this.btnFinaceiro.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnFinaceiro.Depth = 0;
+            this.btnFinaceiro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFinaceiro.Location = new System.Drawing.Point(0, 56);
+            this.btnFinaceiro.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnFinaceiro.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnFinaceiro.Name = "btnFinaceiro";
+            this.btnFinaceiro.Primary = false;
+            this.btnFinaceiro.Size = new System.Drawing.Size(150, 36);
+            this.btnFinaceiro.TabIndex = 1;
+            this.btnFinaceiro.Text = "Financeiro";
+            this.btnFinaceiro.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.AutoSize = true;
+            this.btnCadastrar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCadastrar.Depth = 0;
+            this.btnCadastrar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrar.Location = new System.Drawing.Point(0, 20);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnCadastrar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Primary = false;
+            this.btnCadastrar.Size = new System.Drawing.Size(150, 36);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // lblAlmoço
+            // 
+            this.lblAlmoço.AutoSize = true;
+            this.lblAlmoço.Depth = 0;
+            this.lblAlmoço.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblAlmoço.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblAlmoço.Location = new System.Drawing.Point(402, 446);
+            this.lblAlmoço.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblAlmoço.Name = "lblAlmoço";
+            this.lblAlmoço.Size = new System.Drawing.Size(62, 19);
+            this.lblAlmoço.TabIndex = 1;
+            this.lblAlmoço.Text = "Almoço";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(593, 446);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(51, 19);
+            this.materialLabel1.TabIndex = 2;
+            this.materialLabel1.Text = "Venda";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(463, 427);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(62, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Almoço";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(378, 395);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(62, 19);
+            this.materialLabel3.TabIndex = 4;
+            this.materialLabel3.Text = "Almoço";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(149, 194);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(747, 330);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // frmFinances
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 626);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.lblAlmoço);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmFinances";
+            this.Text = "Financias";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialLabel lblAlmoço;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialFlatButton btnFinaceiro;
+        private MaterialSkin.Controls.MaterialFlatButton btnCadastrar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
